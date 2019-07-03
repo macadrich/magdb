0c0478124b52577285be23d521957d712cead02a
azitra <adriel.artiza@gmail.com>
2019-07-03 21:11:17 +0800

    remove pkg dynamodb

--- magdb.go
+++ magdb.go
@@ -1,12 +1,12 @@
 package magdb
 
 import (
-	"github.com/macadrich/magdb"
+	"github.com/macadrich/go-mcdb/mcdb"
 )
 
 // DataStore inheret to dynamodb
 type DataStore interface {
-	magdb.dynamodb.Datastore
+	mcdb.Datastore
 }
 
 // MagDB instance
@@ -25,10 +25,10 @@ func NewMagDB(region string, tableName string) *MagDB {
 
 // InitDynamoDBConnection initialize dynamodb connection
 // with region and table name
-func (mag *MagDB) InitDynamoDBConnection() (*dynamodb.DB, error) {
-	conn, err := dynamodb.CreateConnection(mag.Region)
+func (mag *MagDB) InitDynamoDBConnection() (*mcdb.DB, error) {
+	conn, err := mcdb.CreateConnection(mag.Region)
 	if err != nil {
 		return nil, err
 	}
-	return dynamodb.NewDynamoDB(conn, mag.TableName), nil
+	return mcdb.NewDynamoDB(conn, mag.TableName), nil
 }
