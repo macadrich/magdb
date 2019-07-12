69c894c276ecb8d2f1fd5c0d48df750f11cfeb7f
azitra <adriel.artiza@gmail.com>
2019-07-12 14:50:34 +0800

    update test

--- magdb_test.go
+++ magdb_test.go
@@ -4,16 +4,6 @@ import (
 	"testing"
 )
 
-type User struct {
-	ID         string `json:"id"`
-	UserName   string `json:"username"`
-	Password   string `json:"password"`
-	Email      string `json:"email"`
-	FirstName  string `json:"firstname"`
-	LastName   string `json:"lastname"`
-	MiddleName string `json:"middlename"`
-}
-
 func TestMongoDB(t *testing.T) {
 	mdb := NewMagDBMongo("localhost", "username", "password", "database name", "source")
 	db, err := mdb.InitMongoDBConnection()
